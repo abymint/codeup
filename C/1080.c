6c16341188e7b21488778cc5bb0f1bae3b64c343
Woogyeong Jung <woogyeong45@gmail.com>
2023-08-09 08:28:35 +0900

    Update 1080.c

--- C/1080.c
+++ C/1080.c
@@ -8,18 +8,11 @@ int main(){
         sum+=i;
         if(sum>=num) break;
     }
+    // while(sum<num){
+    //     i++;
+    //     sum+=i;
+    // }
     printf("%d",i);
     return 0;
 }
-// #include<stdio.h>
-// #include<stdbool.h>
-// int main(){
-//     int sum=0,i=0,num;
-//     scanf("%d",&num);
-//     while(sum<num){
-//         i++;
-//         sum+=i;
-//     }
-//     printf("%d",i);
-//     return 0;
-// }
+
