3580e7260d2854c37759e425529e58912ba9b48a
woogyeong45 <woogyeong45@gmail.com>
2023-08-12 09:16:08 +0900

    코드 수정

--- C/1099-1.c
+++ C/1099-1.c
@@ -1,39 +1,28 @@
 #include<stdio.h>
+#include<stdbool.h>
 int main(){
-    // 변수 선언
-    int x=2,y=2,data[20][20]={};
-    // 입력
-    printf("\n입력 ---------------------------------\n\n");
-    for(int i=1;i<=10;i++)
-        for(int j=1;j<=10;j++)
-            scanf("%d",&data[j][i]);
-    if(data[x][y]==0) data[x][y]=9;
-    // 처리
-    while(1){
-        if(data[x+1][y]==0) data[++x][y]=9;
-        else if(data[x][y+1]==0) data[x][++y]=9;
-        else if(data[x+1][y]==2){
-            data[++x][y]=9;
-            break;
-        }
-        else if(data[x][y+1]==2){
-            data[x][++y]=9;
-            break;
-        }
-        /*printf("\n 진행 과정 ----------------------------\n\n");
-            for(int i=1;i<=10;i++){
-                for(int j=1;j<=10;j++){
-                    printf("%d ",data[j][i]);
-            }
-        printf("\n");
-        }*/
-    }
-    // 출력
-    printf("\n출력 ---------------------------------\n\n");
-     for(int i=1;i<=10;i++){
-        for(int j=1;j<=10;j++)
-            printf("%d ",data[j][i]);
-        printf("\n");
-    }
-    return 0;
+	int map[15][15]={'\0'},x=2,y=2,temp;
+
+	for(int i=1;i<=10;i++)
+		for(int j=1;j<=10;j++)
+			scanf("%d",&map[j][i]);
+
+	while(true){
+		if(map[x][y]==2){
+			map[x][y]=9;
+			break;
+		}
+		else map[x][y]=9;
+		
+		if(map[x+1][y]!=1) x++;
+		else if(map[x][y+1]!=1) y++;
+		else break;
+	}
+	
+	for(int i=1;i<=10;i++){
+		for(int j=1;j<=10;j++)
+			printf("%d ",map[j][i]);
+		printf("\n");
+	}
+	return 0;
 }
